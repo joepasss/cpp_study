60c3be6448999ab86c57a97b1980f4197c00cdbb
Miller <farwes1180@gmail.com>
2022-09-28 08:37:00 +0900

    calc volume and base area

--- operations/main.cpp
+++ operations/main.cpp
@@ -16,14 +16,30 @@
 #include "weird_integral_types.cpp"
 #include "celcius_to_fahernheit.cpp"
 
-using namespace std;
+// base area = width * length
+// volume = base_area * height
 
 int main() {
-  double cel {0};
-  cout << "Please enter a degree value in Celsius : ";
-  cin >> cel;
+  double length {0};
+  double width {0};
+  double height {0};
 
-  cout << cel << " Celsius is " << celToFah(cel) << " Fahrenheit" << endl;
+  std::cout << "Welcome to box calculator. Please type in length, width and height information : " << std::endl;
+
+  std::cout << "length : ";
+  std::cin >> length;
+
+  std::cout << "width : ";
+  std::cin >> width;
+
+  std::cout << "height : ";
+  std::cin >> height;
+
+  double base_area = width * length;
+  std::cout << "The base area is : " << base_area << std::endl;
+
+  double volume = base_area * height;
+  std::cout << "The volume is : " << volume << std::endl;
 
   return 0;
 }

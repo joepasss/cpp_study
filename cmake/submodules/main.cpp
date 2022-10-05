e5b7b33a0e301147329551df3fa3f63b84a3a85e
Miller <farwes1180@gmail.com>
2022-10-06 03:12:15 +0900

    glfw submodules

--- cmake/submodules/main.cpp
+++ cmake/submodules/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
   std::cout << "Hello from SUBMODULES!" << std::endl;
+
+  GLFWwindow* window;
+
+  if(!glfwInit()) {
+    fprintf( stderr, "Failed to initialize GLFW\n" );
+    exit(EXIT_FAILURE);
+  }
+
+  window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+
+  if(!window) {
+    fprintf(stderr, "Failed to open GLFW window\n");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  while(!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
 
   return 0;
 }

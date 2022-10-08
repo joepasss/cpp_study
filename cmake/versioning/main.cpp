4a66b24626cc091f3c5cf61c2f3a92a58d57647a
Miller <farwes1180@gmail.com>
2022-10-08 22:38:13 +0900

    project versioning

--- cmake/versioning/main.cpp
+++ cmake/versioning/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <VERSIONINGConfig.h>
 
-int main() {
+int main(int argc, char* argv[]) {
   std::cout << "HELLO FROM VERSIONING!" << std::endl;
   
+  std::cout << argv[0] << " Version " << VERSIONING_VERSION_MAJOR << "." << VERSIONING_VERSION_MINOR << std::endl;
+
   return 0;
 }

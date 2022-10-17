3577d737e7be276d7d262a4b93b8cc96333d7ccd
Miller <farwes1180@gmail.com>
2022-10-18 07:48:33 +0900

    add library

--- cmake/optional_libs/main.cpp
+++ cmake/optional_libs/main.cpp
@@ -1,9 +1,11 @@
+#include <adder.h>
 #include <iostream>
 
 using namespace std;
 
 int main() {
   cout << "HELLO from optional libs" << endl;
+  cout << "ADDER: " << adder(3.14, 2.13) << endl;
 
   return 0;
 }

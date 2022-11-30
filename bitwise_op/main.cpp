e9f4628438f1e1680df73f967920c37483f0f043
Miller <farwes1180@gmail.com>
2022-11-30 10:27:16 +0900

    shitt operatior

--- bitwise_op/main.cpp
+++ bitwise_op/main.cpp
@@ -8,6 +8,7 @@ void divider() {
 }
 
 void print_int_in_binary();
+void shift_op();
 
 int main() {
   std::cout << "Hello from BITWISE_OP" << std::endl;
@@ -15,8 +16,10 @@ int main() {
 
   std::cout << "Print integer in Binary" << std::endl;
   print_int_in_binary();
-
   divider();
+
+  std::cout << "Shift Operators" << std::endl;
+  shift_op();
 
   return 0;
 }
@@ -30,4 +33,41 @@ void print_int_in_binary() {
   std::cout << "data (dec): " << std::dec << data << std::endl;
   std::cout << "data (hex): " << std::hex << data << std::endl;
   std::cout << "data (bin): " << std::bitset<16>(data) << std::endl;
+}
+
+void shift_op() {
+  // >>
+  // 001111000010 -> 011110000100
+  // <<
+  // 001111000010 -> 000111100001
+
+  // if data is lost as a result of you shifting bits left(<<) or right(>>), you
+  // can't get the data back just by doing reverse operation. You've just lost
+  // the data permanently!
+
+  // bit shifting only supported for integral types like int char, ...
+
+  unsigned short int value{0xff0u};
+
+  std::cout << "value: " << std::bitset<16>(value) << ", dec" << value
+            << std::endl;
+
+  std::cout << std::endl;
+  std::cout << "Shifting 1bit right (>>)" << std::endl;
+
+  // shift right 1 bit position
+  value = static_cast<unsigned short int>(value >> 1);
+  std::cout << "value: " << std::bitset<16>(value) << ", dec" << value
+            << std::endl;
+
+  std::cout << std::endl;
+  std::cout << "Shifting 4bit left (<<)" << std::endl;
+
+  // shift right 4 bit position
+  value = static_cast<unsigned short int>(value << 4);
+  std::cout << "value: " << std::bitset<16>(value) << ", dec" << value
+            << std::endl;
+
+  // shifting right divides by 2^n
+  // shifting left multiplies by 2^n
 }

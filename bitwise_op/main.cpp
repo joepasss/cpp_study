57cca6ff4e406cb9419ba7a7bce88431dcb945bd
Miller <farwes1180@gmail.com>
2022-11-30 09:54:19 +0900

    change structure

--- bitwise_op/main.cpp
+++ bitwise_op/main.cpp
@@ -7,16 +7,7 @@ void divider() {
   std::cout << std::endl;
 }
 
-void print_int_in_binary() {
-  unsigned short int data{0b11111110};
-
-  std::cout << "sizeof unsigned short int: " << sizeof(data)
-            << std::endl; // 2 byte (16 bits)
-
-  std::cout << "data (dec): " << std::dec << data << std::endl;
-  std::cout << "data (hex): " << std::hex << data << std::endl;
-  std::cout << "data (bin): " << std::bitset<16>(data) << std::endl;
-}
+void print_int_in_binary();
 
 int main() {
   std::cout << "Hello from BITWISE_OP" << std::endl;
@@ -28,4 +19,15 @@ int main() {
   divider();
 
   return 0;
+}
+
+void print_int_in_binary() {
+  unsigned short int data{0b11111110};
+
+  std::cout << "sizeof unsigned short int: " << sizeof(data)
+            << std::endl; // 2 byte (16 bits)
+
+  std::cout << "data (dec): " << std::dec << data << std::endl;
+  std::cout << "data (hex): " << std::hex << data << std::endl;
+  std::cout << "data (bin): " << std::bitset<16>(data) << std::endl;
 }
